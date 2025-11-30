9f7fa9717af4a7693f22085c0bb5fd7f47af06b0
Oliver <contact@oljver.dev>
2025-11-30 19:19:04 +0100

    Refactor ETAHud to simplify error handling and remove unnecessary chat messages

--- src/main/java/org/kybe/ETAHud.java
+++ src/main/java/org/kybe/ETAHud.java
@@ -17,14 +17,11 @@ public class ETAHud extends TextHudElement {
 	private int glDurability = 0;
 	private volatile boolean running = false;
 	private Thread updateThread;
-	Component prefix = Component.literal("[ELYTRA ETA] ").withStyle(ChatFormatting.RED);
 
 	final NumberSetting<Integer> delay = new NumberSetting<Integer>("Delay ms", "Check delay", 500, 0, 10000)
 			.incremental(1)
 			.onChange(setting -> {
 				if (this.running) {
-					// Print to chat that the delay has been changed
-					mc.gui.getChat().addMessage(Component.empty().append(prefix).append(Component.literal("Delay changed to " + setting.longValue() + "ms").withStyle(ChatFormatting.WHITE)));
 					stopUpdateThread();
 					startUpdateThread();
 				}
@@ -37,17 +34,11 @@ public class ETAHud extends TextHudElement {
 
 	@Override
 	public String getText() {
-		if (mc.level == null) {
-			mc.gui.getChat().addMessage(Component.empty().append(prefix).append(Component.literal("MC.LEVEL NULL").withStyle(ChatFormatting.WHITE)));
-			return "ETA: ERROR";
-		}
-
-		if (!this.isToggled()) {
+		if (mc.level == null || !this.isToggled()) {
 			return "ETA: ERROR";
 		}
 
 		if (!running) {
-			mc.gui.getChat().addMessage(Component.empty().append(prefix).append(Component.literal("STARTED").withStyle(ChatFormatting.WHITE)));
 			startUpdateThread();
 		}
 
@@ -58,7 +49,6 @@ public class ETAHud extends TextHudElement {
 	@Override
 	public void onDisable() {
 		stopUpdateThread();
-		mc.gui.getChat().addMessage(Component.empty().append(prefix).append(Component.literal("STOPPED").withStyle(ChatFormatting.WHITE)));
 		super.onDisable();
 	}
 
@@ -71,7 +61,6 @@ public class ETAHud extends TextHudElement {
 					Thread.sleep(delay.getValue());
 				}
 			} catch (InterruptedException e) {
-				this.getLogger().error("ETA calculation thread interrupted", e);
 				Thread.currentThread().interrupt();
 			} catch (Exception e) {
 				this.getLogger().error("Unexpected error in ETA calculation thread", e);
